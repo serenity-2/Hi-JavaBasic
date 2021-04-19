5ba5f6fc85e78c082921cefb874087f32fa41419
qianchangqing.ext <756974950@qq.com>
2021-04-19 14:44:12 +0800

    添加opinional测试类

---
+++ src/test/java/com/jzjr/inc/java_basic/OptionalTest.java
@@ -0,0 +1,99 @@
+package com.jzjr.inc.java_basic;
+
+import com.jzjr.inc.java_basic.bean.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+@Slf4j
+@SpringBootTest
+public class OptionalTest {
+    /**
+     * optional类
+     */
+    @Test
+    public void print1(){
+        //创建一个空的optional
+        Optional<Object> empty = Optional.empty();
+        //java.util.NoSuchElementException: No value present
+        empty.get();
+    }
+
+    @Test
+    public void print2(){
+        //创建一个包含值的optional，不能传入null类型
+        Optional<User> optionalUser = Optional.of(new User("Daisy", 24));
+        User user = optionalUser.get();
+
+        User user1 = null;
+        Optional<User> user2 = Optional.of(user1);
+        //java.lang.NullPointerException
+        System.out.println(user2.get());
+    }
+
+    @Test
+    public void print3(){
+        //创建一个包含值的optional，可以传入null类型
+        User user = null;
+        Optional<User> user1 = Optional.ofNullable(user);
+        //Optional.empty
+        System.out.println(user1);
+    }
+
+    @Test
+    public void print4(){
+        //ifPresent 如果存在一个值，则使用该值调用指定的使用者，否则什么也不做
+        Optional<User> optionalUser = Optional.of(new User("Daisy", 24));
+        optionalUser.ifPresent(user -> {
+            Assertions.assertEquals(user.getUsername(),"moko");
+        });
+    }
+
+    @Test
+    public void print5(){
+        //ofElse 返回默认值，如果有值则返回该值，否则返回传递给它的参数值
+        User user = null;
+        User user1 = Optional.ofNullable(user).orElse(new User("Daisy", 24));
+        System.out.println(user1);
+    }
+
+    @Test
+    public void print6() {
+        User user = null;
+        //当对象为空时都返回默认对象
+        log.info("Using orElse");
+        User result = Optional.ofNullable(user).orElse(createNewUser());
+        log.info("Using orElseGet");
+        User result2 = Optional.ofNullable(user).orElseGet(() -> createNewUser());
+    }
+
+    private User createNewUser() {
+        log.info("Creating New User");
+        return new User("extra@gmail.com", 24);
+    }
+
+    @Test
+    public void print7() {
+        User user = new User("john@gmail.com", 24);
+        log.info("Using orElse");
+        //orElse即使有值也会执行形参方法
+        User result = Optional.ofNullable(user).orElse(createNewUser());
+        log.info("Using orElseGet");
+        //orElseGet有值不会执行传入的supplier函数
+        User result2 = Optional.ofNullable(user).orElseGet(() -> createNewUser());
+    }
+
+    @Test
+    public void print8(){
+        //orElseThrow 在对象为空时抛出异常
+        User user = null;
+        try {
+            User result = Optional.ofNullable(user).orElseThrow( () -> new IllegalArgumentException());
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
