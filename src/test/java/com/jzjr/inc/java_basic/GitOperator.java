66d9271768c7c3579f26cc17a6a4de4cfaeadf62
qianchangqing.ext <756974950@qq.com>
2021-08-05 14:36:23 +0800

    20200000 third commit

---
+++ src/test/java/com/jzjr/inc/java_basic/GitOperator.java
@@ -0,0 +1,12 @@
+package com.jzjr.inc.java_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GitOperator {
+    @Test
+    public void gitBranch() {
+        System.out.println("20200000-dev");
+    }
+}
