f2b83c91906c3150ade6443b4671d2ad7352db61
qianchangqing.ext <756974950@qq.com>
2021-08-05 15:30:02 +0800

    20200000 forth commit

---
+++ src/test/java/com/jzjr/inc/java_basic/Earth.java
@@ -0,0 +1,12 @@
+package com.jzjr.inc.java_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class Earth {
+    @Test
+    public void test() {
+        System.out.println("earth is only home for human");
+    }
+}
